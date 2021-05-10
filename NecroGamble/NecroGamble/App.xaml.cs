c69a9a52f010a04be5f558c9ea854d7aaf96f195
albemu07 <albemu07@ucm.es>
2021-05-11 01:59:47 +0200

    Mas cambios a mis menus

--- NecroGamble/NecroGamble/App.xaml.cs
+++ NecroGamble/NecroGamble/App.xaml.cs
@@ -66,7 +66,7 @@ namespace NecroGamble
                     // Cuando no se restaura la pila de navegación, navegar a la primera página,
                     // configurando la nueva página pasándole la información requerida como
                     //parámetro de navegación
-                    rootFrame.Navigate(typeof(MenuPreparacion), e.Arguments);
+                    rootFrame.Navigate(typeof(Options), e.Arguments);
                 }
                 // Asegurarse de que la ventana actual está activa.
                 Window.Current.Activate();
