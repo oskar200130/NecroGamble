5a9849450a323205541cd381aebbdb31d0f74706
oscar <osgarc05@ucm.es>
2021-05-19 22:00:07 +0200

    Menu de partida funcionando a falta de la pausa

--- NecroGamble/NecroGamble/GameMenu.xaml.cs
+++ NecroGamble/NecroGamble/GameMenu.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,30 +25,108 @@ namespace NecroGamble
     public sealed partial class GameMenu : Page
     {
         Random rand = new Random();
+        private bool dadoIzq = true;
+        private bool dadoDcho = true;
+        private int numDados = 0;
+        private int turn = 0;
 
         public GameMenu()
         {
             this.InitializeComponent();
+            Turn1.Background = new SolidColorBrush(Colors.Red);
         }
 
         private void Moneda_Click(object sender, RoutedEventArgs e)
         {
-            int i = rand.Next(0, 2);
-            if (i == 0)
+            if (ButtonsTarget.Text == "")
             {
-                //if (ButtonsTarget.Text != "Tira solo uno")
-                //{
+                dadoDcho = dadoIzq = true;
+                int i = rand.Next(0, 2);
+                if (i == 0)
+                {
+                    //if (ButtonsTarget.Text != "Tira solo uno")
+                    //{
                     Moneda.Source = new BitmapImage(new Uri("ms-appx:///Assets/PartidaMenu/monedaCara.png"));
                     ButtonsTarget.Text = "Tira solo uno";
-                //}
-            }
-            else
-            {
-                //if (ButtonsTarget.Text != "Tira ambos")
-                //{
+                    numDados = 1;
+                    //}
+                }
+                else
+                {
+                    //if (ButtonsTarget.Text != "Tira ambos")
+                    //{
                     Moneda.Source = new BitmapImage(new Uri("ms-appx:///Assets/PartidaMenu/monedaCruz.png"));
                     ButtonsTarget.Text = "Tira ambos";
-                //}
+                    numDados = 2;
+                    //}
+                }
+            }
+        }
+
+        private void WalkDice_Click(object sender, RoutedEventArgs e)
+        {
+            if (dadoIzq)
+            {
+                numDados--;
+                if (numDados == 0) DiceAcction();
+                dadoIzq = false;
+            }
+        }
+
+        private void AttackDice_Click(object sender, RoutedEventArgs e)
+        {
+            if (dadoDcho)
+            {
+                numDados--;
+                if (numDados == 0) DiceAcction();
+                dadoDcho = false;
+            }
+        }
+
+        private void DiceAcction()
+        {
+            infoText.FontSize = 40;
+            infoText.Foreground = new SolidColorBrush(Colors.Black);
+            infoText.FontFamily = new FontFamily("Aclonica");
+
+            ButtonsTarget.Text = "";
+            turn++;
+            if (turn > 3) turn = 0;
+            switch (turn)
+            {
+                case 0:
+                    Turn4.Background = new SolidColorBrush(Colors.Transparent);
+                    Turn1.Background = new SolidColorBrush(Colors.Red);
+                    TurnArrow4.Visibility = Visibility.Collapsed;
+                    TurnArrow1.Visibility = Visibility.Visible;
+                    infoImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/PartidaMenu/arquero.png"));
+                    infoText.Text = "Hola";
+                    
+                    break;
+                case 1:
+                    Turn1.Background = new SolidColorBrush(Colors.Transparent);
+                    Turn2.Background = new SolidColorBrush(Colors.Red);
+                    TurnArrow1.Visibility = Visibility.Collapsed;
+                    TurnArrow2.Visibility = Visibility.Visible;
+                    infoImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/PartidaMenu/muerte.png"));
+                    infoText.Text = "Tus";
+                    break;
+                case 2:
+                    Turn2.Background = new SolidColorBrush(Colors.Transparent);
+                    Turn3.Background = new SolidColorBrush(Colors.Red);
+                    TurnArrow2.Visibility = Visibility.Collapsed;
+                    TurnArrow3.Visibility = Visibility.Visible;
+                    infoImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/PartidaMenu/caballero.png"));
+                    infoText.Text = "Putos";
+                    break;
+                case 3:
+                    Turn3.Background = new SolidColorBrush(Colors.Transparent);
+                    Turn4.Background = new SolidColorBrush(Colors.Red);
+                    TurnArrow3.Visibility = Visibility.Collapsed;
+                    TurnArrow4.Visibility = Visibility.Visible;
+                    infoImage.Source = new BitmapImage(new Uri("ms-appx:///Assets/PartidaMenu/tercio.png"));
+                    infoText.Text = "Muertos";
+                    break;
             }
         }
     }
